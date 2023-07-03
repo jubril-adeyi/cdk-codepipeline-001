6ffc2d656bd7babf6ed1f86a1861608ef67a8819
Jubril Adeyi <jadeyi@gmail.com>
2023-07-03 13:03:14 +0100

    initialized cdk

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from codepipline.codepipline_stack import CodepiplineStack
+
+
+app = cdk.App()
+CodepiplineStack(app, "CodepiplineStack",
+    )
+
+app.synth()

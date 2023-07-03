6ec4e76d830bbe5cdb7debcd181bf6ca20526704
Jubril Adeyi <jadeyi@gmail.com>
2023-07-03 14:25:52 +0100

    Modified CodepipelineStack with source stage and build stage

--- codepipeline/codepipeline_stack.py
+++ codepipeline/codepipeline_stack.py
@@ -4,7 +4,7 @@ from aws_cdk import (
     aws_codepipeline_actions as codepipeline_actions,
     aws_codebuild as codebuild,
     aws_codecommit as codecommit,
-
+    aws_iam as iam,
 )
 from constructs import Construct
 
@@ -49,12 +49,51 @@ class CodepipelineStack(Stack):
 
         #BUILD
 
-        cfn_lint_project_name = "cfn_lint_project"  # Replace with your CodeBuild project name
-        cfn_lint_project = codebuild.Project.from_project_name(
+        # Define existing build project 
+        # cfn_lint_project_name = "cfn-lint"  # Replace with your CodeBuild project name
+        # cfn_lint_project = codebuild.Project.from_project_name(
+        #     self,
+        #     "MyCodeBuildProject",
+        #     cfn_lint_project_name
+        # )
+
+        # Define new build
+
+        # Define CodeBuild project with custom Docker image
+        custom_image = "slickboy/cdk-nodejs:1.1"
+        existing_role_arn = "arn:aws:iam::723389358939:role/service-role/codebuild-build-001-service-role"
+        cfn_lint_project = codebuild.Project(
             self,
             "MyCodeBuildProject",
-            cfn_lint_project_name
+            project_name="cfn-lint-project",
+            build_spec=codebuild.BuildSpec.from_object({
+                "version": "0.2",
+                "phases": {
+                    "install": {
+                        "commands": [
+                            "pip3 install cfn-lint"
+                        ]
+                    },
+                    "build": {
+                        "commands": [
+                            "cdk synth >> template.json"
+                            "cfn-lint  template.json",
+                        ]
+                    }
+                }
+            }),
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.from_docker_registry(custom_image),
+                privileged=None  # Set to True if you need elevated privileges
+            ),
+
+            role=iam.Role.from_role_arn(
+                self,
+                "ExistingCodeBuildRole",
+                role_arn=existing_role_arn
+            )
         )
+
         # Define your pipeline build stage - ADD stage to pipline
         self.build_stage = self.pipeline.add_stage(stage_name="Build")
         
